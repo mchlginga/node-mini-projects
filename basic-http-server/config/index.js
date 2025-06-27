f301a441d9670dcc3b9dc90e897acfd3cb2869ff
mchl <mchlginga@gmail.com>
2025-06-27 16:48:38 +0800

    add environment-based config

---
+++ basic-http-server/config/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const dev = require ("./dev.js");
+const prod = require ("./prod.js");
+
+const env = process.env.ENV || "development";
+
+const config = env === "development" ? dev : prod;
+
+module.exports = config;
+
+
